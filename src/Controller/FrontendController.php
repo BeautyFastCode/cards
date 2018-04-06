fd6ed3ba436ebd5566bbd170e1a243fbd89d0ef4
BeautyFastCode.com <beautyfastcode@gmail.com>
2018-04-06 23:30:13 +0200

    first look at the templates for the frontend and dashboard, adding Tailwind CSS

---
+++ src/Controller/FrontendController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class FrontendController extends Controller
+{
+    /**
+     * @Route("/", name="frontend")
+     */
+    public function index()
+    {
+        return $this->render('frontend/index.html.twig');
+    }
+}
