996a971b068a6a56b9678494dc591d35e2562295
BeautyFastCode.com <beautyfastcode@gmail.com>
2018-04-09 15:04:07 +0200

    adding sample data for suite

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -12,6 +12,40 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return $this->render('dashboard/index.html.twig');
+        $suites = [
+            [
+                'name'  => 'Suite A',
+                'decks' => [
+                    [
+                        'name'       => 'Untitled Deck',
+                        'background' => 'bg-secondary',
+                    ],
+                    [
+                        'name'       => 'Welcome Deck',
+                        'background' => 'bg-success',
+                    ],
+                    [
+                        'name'       => 'Information Deck',
+                        'background' => 'bg-info',
+                    ],
+                ],
+            ],
+            [
+                'name'  => 'Calendar',
+                'decks' => [
+                    [
+                        'name'       => '2018 - 04',
+                        'background' => 'bg-danger',
+                    ],
+                    [
+                        'name'       => 'Project Cards',
+                        'background' => 'bg-warning',
+                    ],
+                ],
+            ],
+            ['name' => 'Empty Suite'],
+        ];
+
+        return $this->render('dashboard/index.html.twig', ['suites' => $suites]);
     }
 }
